b56a19634876c9e2c366bc04ae8cfe495f59b516
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-07 19:47:10 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,5 +10,4 @@ CUDA_VISIBLE_DEVICES=0 python src/run.py --argoverse --future_frame_num 30 \
     stage_one stage_one_dynamic=0.95 laneGCN-4 point_level point_level-4 \
     point_level-4-3 complete_traj complete_traj-3 \
   --model_recover_path EV2_0_add_lane_gcn/model_save/model.16.bin \
-  --reuse_temp_file --temp_file_dir output \
   --do_eval --eval_params optimization MRminFDE cnt_sample=9 opti_time=0.1 --eval_batch_size 8
