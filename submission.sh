54ce0fb919229cff600a3e3a16db8a7cd58f0fbb
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-14 16:12:21 +0800

    Update submission.sh

--- submission.sh
+++ submission.sh
@@ -9,5 +9,5 @@ CUDA_VISIBLE_DEVICES=0 python src/do_test.py --argoverse --future_frame_num 30 \
     goals_2D enhance_global_graph subdivide lazy_points new laneGCN point_sub_graph \
     stage_one stage_one_dynamic=0.95 laneGCN-4 point_level point_level-4 \
     point_level-4-3 complete_traj complete_traj-3 \
-  --model_recover_path EV2_0_add_lane_gcn/model_save/model.16.bin \
+  --model_recover_path submission/model.16.bin \
   --do_eval --do_test --eval_params optimization MRminFDE cnt_sample=9 opti_time=0.1 --eval_batch_size 8
