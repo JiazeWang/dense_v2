3994c1c41506c276f5cb1f41ffa12e5df3e77578
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-15 09:11:18 +0800

    Update submission.sh

--- submission.sh
+++ submission.sh
@@ -10,4 +10,4 @@ CUDA_VISIBLE_DEVICES=0 python src/do_test.py --argoverse --future_frame_num 30 \
     stage_one stage_one_dynamic=0.95 laneGCN-4 point_level point_level-4 \
     point_level-4-3 complete_traj complete_traj-3 \
   --model_recover_path submission/model.16.bin \
-  --do_eval --do_test --eval_params optimization MRminFDE cnt_sample=9 opti_time=0.1 --eval_batch_size 8
+  --do_eval --do_test --eval_params optimization MRminFDE=0.0 cnt_sample=9 opti_time=0.1 --eval_batch_size 8
