f2a136fb7cdf3d4246447d9eb285a6c3d3c752de
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-16 20:54:45 +0800

    Update decoder.py

--- src/modeling/decoder.py
+++ src/modeling/decoder.py
@@ -228,18 +228,18 @@ class Decoder(nn.Module):
                 predict_traj = self.complete_traj_decoder(
                     torch.cat([hidden_states[i, 0, :].detach(), target_feature, hidden_attention], dim=-1)).view(
                     [self.future_frame_num, 2])
-                print(hidden_states[i, 0, :].detach().shape, target_feature.shape, hidden_attention.shape)
-                print("predict_traj.shape:", predict_traj.shape)
+                #print(hidden_states[i, 0, :].detach().shape, target_feature.shape, hidden_attention.shape)
+                #print("predict_traj.shape:", predict_traj.shape)
             loss[i] += (F.smooth_l1_loss(predict_traj, torch.tensor(gt_points, dtype=torch.float, device=device), reduction='none') * \
                         torch.tensor(labels_is_valid[i], dtype=torch.float, device=device).view(self.future_frame_num, 1)).mean()
-
-        # Refinement Module
-        input_feature = torch.cat([hidden_states[i, 0, :].detach(), target_feature, hidden_attention], dim=-1)
-        predict_traj = self.Refiner(input_feature, predict_traj)
-        print("predict_traj:", predict_traj.shape)
-        print("refine_target.shape:", refine_target.shape)
-        print("scores.shape", scores.shape, "torch.tensor([mapping[i]['goals_2D_labels']]:", torch.tensor([mapping[i]['goals_2D_labels']]))
-        print(TestEnd)
+            # Refinement Module
+            input_feature = torch.cat([hidden_states[i, 0, :].detach(), target_feature, hidden_attention], dim=-1)
+            predict_traj = self.Refiner(input_feature, predict_traj)
+            loss[i] += (F.smooth_l1_loss(predict_traj, torch.tensor(gt_points, dtype=torch.float, device=device), reduction='none') * \
+                        torch.tensor(labels_is_valid[i], dtype=torch.float, device=device).view(self.future_frame_num, 1)).mean()
+            #print("predict_traj:", predict_traj.shape)
+            #print("scores.shape", scores.shape, "torch.tensor([mapping[i]['goals_2D_labels']]:", torch.tensor([mapping[i]['goals_2D_labels']]))
+            #print(TestEnd)
         loss[i] += F.nll_loss(scores.unsqueeze(0),
                               torch.tensor([mapping[i]['goals_2D_labels']], device=device))
 
