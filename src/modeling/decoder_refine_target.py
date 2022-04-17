9a33eb3e2617db3052d6b83f58b6a2fde1eab039
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-17 20:01:44 +0800

    Update decoder_refine_target.py

--- src/modeling/decoder_refine_target.py
+++ src/modeling/decoder_refine_target.py
@@ -223,7 +223,7 @@ class Decoder(nn.Module):
 
             input_feature = torch.cat([hidden_states[i, 0, :].detach(), target_feature, hidden_attention], dim=-1)
             predict_target = self.TRefiner(input_feature, torch.tensor(highest_goal, dtype=torch.float, device=device))
-            print(predict_target_feature.shape, torch.tensor(gt_points[final_idx], dtype=torch.float, device=device).shape)
+            print(predict_target.shape, torch.tensor(gt_points[final_idx], dtype=torch.float, device=device).shape)
             loss[i] += (F.smooth_l1_loss(highest_goal, torch.tensor(gt_points[final_idx], dtype=torch.float, device=device)))
             print("refine_loss:", (F.smooth_l1_loss(highest_goal, torch.tensor(gt_points[final_idx], dtype=torch.float, device=device))))
 
