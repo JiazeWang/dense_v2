43922f90aa79d64b340d86ac2bcc046f35d510e7
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-19 09:36:02 +0800

    Update decoder_voting.py

--- src/modeling/decoder_voting.py
+++ src/modeling/decoder_voting.py
@@ -194,7 +194,7 @@ class Decoder(nn.Module):
                         torch.tensor(labels_is_valid[i], dtype=torch.float, device=device).view(self.future_frame_num, 1)).mean()
 
         offsets_gt = torch.tensor(gt_points[final_idx] - goals_2D,  device=device, dtype=torch.float)
-        print("offsets_gt.shape", offsets_gt)
+        print("offsets_gt.shape", offsets_gt[0].min(), offsets_gt[1].min())
         print(TESTEND)
         loss[i] += F.nll_loss(scores.unsqueeze(0),
                               torch.tensor([mapping[i]['goals_2D_labels']], device=device))
