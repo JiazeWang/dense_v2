617d24900563769f9650746c1577761d6281411f
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-24 18:29:19 +0800

    Update vectornet.py

--- src/modeling/vectornet.py
+++ src/modeling/vectornet.py
@@ -5,7 +5,8 @@ import torch
 import torch.nn.functional as F
 from torch import nn, Tensor
 
-from modeling.decoder import Decoder, DecoderResCat
+from modeling.decoder_origin import Decoder, DecoderResCat
+#from modeling.decoder import Decoder, DecoderResCat
 #from modeling.decoder_refine_target import Decoder, DecoderResCat
 #from modeling.decoder_voting import Decoder, DecoderResCat
 from modeling.lib import MLP, GlobalGraph, LayerNorm, CrossAttention, GlobalGraphRes_h
