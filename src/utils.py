115a94a4b4ea6e55747061fb402f92f923187051
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-25 16:00:49 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1254,6 +1254,7 @@ def get_neighbour_points_dense(points, topk_ids=None, mapping=None, neighbour_di
     for fake_idx, point in enumerate(points):
         x, y = round(float(point[0])), round(float(point[1]))
         granularity = int(5 - np.floor(4 * fake_idx/num))
+        print(granularity)
         # not compatible argo
         for i in range(-neighbour_dis * granularity, (neighbour_dis + 1)* granularity):
             for j in range(-neighbour_dis * granularity, (neighbour_dis + 1)* granularity):
