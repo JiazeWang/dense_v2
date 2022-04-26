9eef814fe992bbc7f490dd22173d55d63087344b
JiazeWang <jzwang@link.cuhk.edu.hk>
2022-04-26 11:24:33 +0800

    Update decoder_origin.py

--- src/modeling/decoder_origin.py
+++ src/modeling/decoder_origin.py
@@ -140,11 +140,11 @@ class Decoder(nn.Module):
             k = 40
         _, topk_ids = torch.topk(scores, k=min(k, len(scores)))
         topk_ids = topk_ids.tolist()
-
-        goals_2D_new = utils.get_neighbour_points_dense(goals_2D[topk_ids], topk_ids=topk_ids, mapping=mapping[i])
+        goals_2D_new = utils.get_neighbour_points(goals_2D[topk_ids], topk_ids=topk_ids, mapping=mapping[i])
+        #goals_2D_new = utils.get_neighbour_points_dense(goals_2D[topk_ids], topk_ids=topk_ids, mapping=mapping[i])
         #print("goals_2D_new[0]:", goals_2D_new[0])
-        print("len(goals_2D_new):", len(goals_2D_new))
-        assert len(goals_2D_new)
+        #print("len(goals_2D_new):", len(goals_2D_new))
+        a#ssert len(goals_2D_new)
         goals_2D_new = torch.cat([torch.tensor(goals_2D_new, device=device, dtype=torch.float),
                                   torch.tensor(goals_2D, device=device, dtype=torch.float)], dim=0)
 
